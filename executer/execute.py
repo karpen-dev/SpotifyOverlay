ade8df7db059997b5b9ed49c7407e0b1b86de395
karpen <119096630+karpen-dev@users.noreply.github.com>
2025-06-07 20:26:41 +0900

    add execute script

--- executer/execute.py
+++ executer/execute.py
@@ -1,3 +1,28 @@
+import subprocess
+import sys
 import os
 
-os.system("java --enable-native-access=javafx.graphics --add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED -jar bin/SpotifyOverlay.jar")
+def run_app():
+    jar_path = os.path.join("bin", "SpotifyOverlay.jar")
+
+    if not os.path.exists(jar_path):
+        print(f"Error, file {jar_path} not found")
+        sys.exit(1)
+
+    command = [
+        "java",
+        "--enable-native-access=javafx.graphics",
+        "--add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED",
+        "-jar",
+        jar_path
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error starting app {e}")
+    except FileNotFoundError:
+        print("Error java not installed")
+
+
+run_app()
